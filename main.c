ffec1224a9d95eb958f12aa7ee88b25ad39e6a75
yeon3ee <2310538@sookmyung.ac.kr>
2023-11-23 13:37:54 +0900

    11/23

--- main.c
+++ main.c
@@ -2,7 +2,7 @@
 //  main.c
 //  SMMarble
 //
-//  Created by Cheyeon Park on 2023/11/05. update on 2023/11/10
+//  Created by Juyeop Kim on 2023/11/05. Cheyeon Park update on 2023/11/10
 //
 
 #include <time.h>
@@ -15,11 +15,21 @@
 #define FOODFILEPATH "marbleFoodConfig.txt"
 #define FESTFILEPATH "marbleFestivalConfig.txt"
 
+#define MAX_PLAYER		100
+
 
 //board configuration parameters
 static int board_nr;
 static int food_nr;
 static int festival_nr;
+
+static int player_nr;
+
+static int player_energy[MAX_PLAYER];
+static int player_position[MAX_PLAYER];
+static int player_name[MAX_PLAYER][MAX_CHARNAME];
+
+
 
 
 
@@ -35,6 +45,24 @@ smmGrade_e takeLecture(int player, char *lectureName, int credit); //take the le
 void* findGrade(int player, char *lectureName); //find the grade from the player's grade history
 void printGrades(int player); //print all the grade history of the player
 #endif
+
+void generatePlayers(int n, int initEnergy) {
+	//n time loop
+	
+	for (i=0;i<n;i++)
+	{
+		//input name
+	printf();//안내문구 
+	scanf("%s",playername[i]);
+	
+	//set position
+	player_position[i]=0;
+	
+	//set energy
+	player_energy[i]=initEnergy;
+	}
+	  
+}
 
 
 
@@ -105,9 +133,10 @@ int main(int argc, const char * argv[]) {
     printf("Total number of board nodes : %i\n", board_nr);
     
     for (i = 0;i<board_nr;i++)
-        printf("node %i : %s, %i\n", i, smmObj_getNodeName(i), smmObj_getNodeType(i));
+        printf("node %i : %s, %i(%s)\n", i, smmObj_getNodeName(i), smmObj_getNodeType(i),smmObj_getTypeName(smmObj_getNodeType(i)));
+        
     
-    #if 0
+    #if0
     //2. food card config 
     if ((fp = fopen(FOODFILEPATH,"r")) == NULL)
     {
@@ -143,15 +172,21 @@ int main(int argc, const char * argv[]) {
     
     
     //2. Player configuration ---------------------------------------------------------------------------------
-    /*
+
     do
     {
         //input player number to player_nr
+        printf("input player no.:");
+		scanf("%d",&player_nr);
+		fflush(stdin);
     }
-    while ();
-    generatePlayers();
-    */
+    while (player_nr<0 || player > MAX_PLAYER);
+	
+	
+	
+    generatePlayers(player_nr, initEnergy);
     
+    /*
     //3. SM Marble game starts ---------------------------------------------------------------------------------
     while () //is anybody graduated?
     {
@@ -161,7 +196,6 @@ int main(int argc, const char * argv[]) {
         //printPlayerStatus();
         
         //4-2. die rolling (if not in experiment)
-        
         
         //4-3. go forward
         //goForward();
